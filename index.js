3b700713148465f49290e0bd64ba77c25d909c4b
swaranjit banerjee <swaranjitbanerjee7@gmail.com>
2026-05-01 20:40:23 +0530

    actions test app

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello World!", uptime: process.uptime() });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
